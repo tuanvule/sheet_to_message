367024d9654e01f075b433e15f475abf51d92963
tuanvule <ngovipmo1@gmail.com>
2025-08-04 09:10:29 +0700

    a

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,13 @@
 importScripts('https://www.gstatic.com/firebasejs/11.7.3/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.7.3/firebase-messaging-compat.js');
 const firebaseConfig = {
-    apiKey: "AIzaSyAhL7Gbr1ZqG0j1KOmMUUJtCN7byc669ss",
-    authDomain: "csvcnhh.firebaseapp.com",
-    projectId: "csvcnhh",
-    storageBucket: "csvcnhh.firebasestorage.app",
-    messagingSenderId: "682386956230",
-    appId: "1:682386956230:web:9e048fd94c8fbbbcb05cc7"
-  };
+  apiKey: "AIzaSyAhL7Gbr1ZqG0j1KOmMUUJtCN7byc669ss",
+  authDomain: "csvcnhh.firebaseapp.com",
+  projectId: "csvcnhh",
+  storageBucket: "csvcnhh.firebasestorage.app",
+  messagingSenderId: "682386956230",
+  appId: "1:682386956230:web:9e048fd94c8fbbbcb05cc7"
+};
 const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging(app);
 
